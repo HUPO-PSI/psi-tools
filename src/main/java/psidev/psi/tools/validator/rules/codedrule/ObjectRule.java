faccff525406b71257d02c40ae2b7576651a3014
samuel-kerrien <samuel.kerrien@gmail.com>
2008-06-26 15:09:21 +0000

    updated javadoc can check is now using the generics type T

--- src/main/java/psidev/psi/tools/validator/rules/codedrule/ObjectRule.java
+++ src/main/java/psidev/psi/tools/validator/rules/codedrule/ObjectRule.java
@@ -1,18 +1,19 @@
 package psidev.psi.tools.validator.rules.codedrule;
 
+import psidev.psi.tools.ontology_manager.OntologyManager;
 import psidev.psi.tools.validator.ValidatorException;
 import psidev.psi.tools.validator.ValidatorMessage;
 import psidev.psi.tools.validator.rules.AbstractRule;
-import psidev.psi.tools.ontology_manager.OntologyManager;
 
 import java.util.Collection;
 
 /**
  * Rule intended to perform custom check on a object of type T.
- * Author: florian, Samuel Kerrien (skerrien@ebi.ac.uk)
- * 
- * @since 1.03
+ *
+ * @author florian Reisinger
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
+ * @since 1.03
  */
 public abstract class ObjectRule<T> extends AbstractRule {
 
@@ -20,8 +21,21 @@ public abstract class ObjectRule<T> extends AbstractRule {
         super( ontologyManager );
     }
 
-    public abstract boolean canCheck( Object object );
+    /**
+     * Verifies if the current rule can apply its check on the given object. Usually that verification is applied to
+     * the Class of the given object.
+     *
+     * @param t the object we want to check on.
+     * @return true is the current rule is able to check on it.
+     */
+    public abstract boolean canCheck( T t );
 
-    public abstract Collection<ValidatorMessage> check(T t ) throws ValidatorException;
-    
+    /**
+     * Validates the given object.
+     *
+     * @param t the object to be validated.
+     * @return a non null collection of messages.
+     * @throws ValidatorException should an error occur in the process of validating the given object.
+     */
+    public abstract Collection<ValidatorMessage> check( T t ) throws ValidatorException;
 }
